b8ecad8f26a6d4e0caeb91065d97c5a44fc7ba87
noahWhit990 <whi24011@byui.edu>
2026-09-18 16:12:43 -0600

    prep assignments 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+        string grade;
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        if (!(grade == "A" || grade == "F"))
+        {
+            if (percent % 10 >= 7)
+            {
+                grade += "+";
+            }
+            else if (percent % 10 <= 3)
+            {
+                grade += "-";
+            }
+        }
+        else if (grade == "A")
+        {
+            if (percent % 10 <= 3 && percent < 100)
+            {
+                grade += "-";
+            }
+        }
+        Console.WriteLine();
+        Console.Write($"Your grade is {grade}. ");
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Better luck next time");
+        }
     }
 }
